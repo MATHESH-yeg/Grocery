4dbf6c47d6d9d0096650a30bb5d2187055ccdacb
Mathesh <matheshwaranmathesh432@gmail.com>
2026-01-18 02:04:53 +0530

    uploaded and final frontend url 8

--- api/products.js
+++ api/products.js
@@ -1,3 +1,7 @@
+
+import mongoose from 'mongoose';
+
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true });
 export default function handler(req, res) {
   res.status(200).json({
     status: "API WORKS"
